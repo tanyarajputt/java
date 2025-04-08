e8a48b4f74e2b0c9cc336028c1943d3e1eac13c2
tanyarajputt <rajputtanya5772gmail.com>
2025-04-08 13:33:31 +0530

    helllo

---
+++ addtion.java
@@ -0,0 +1,9 @@
+public class addtion {
+    public static void main(String[] args){
+        int a = 56;
+        int b = 76;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    
+}
